eea413dc42078d457ed47b2d399c984299ff8086
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-02-04 16:54:50 -0800

    implemented /today & /overview endpoints

--- database/methods/details.js
+++ database/methods/details.js
@@ -2,6 +2,9 @@ const client = require('../index');
 
 const addDetails = (data) => client.query(`INSERT INTO details(current_habit, day_1, day_2, day_3, day_4, day_5, day_6, day_7, time_1, time_2, time_3, time_4) VALUES ('${data.current_habit}', '${data.day_1}', '${data.day_2}', '${data.day_3}', '${data.day_4}', '${data.day_5}', '${data.day_6}', '${data.day_7}', '${data.time_1}', '${data.time_2}', '${data.time_3}', '${data.time_4}')`);
 
+const getDetails = (data) => client.query(`SELECT id, current_habit, time_1, time_2, time_3, time_4 FROM details WHERE day_${data} = true`);
+
 module.exports = {
   addDetails,
+  getDetails,
 };
--- database/methods/habits.js
+++ database/methods/habits.js
@@ -1,7 +1,10 @@
 const client = require('../index');
 
-const addHabit = (data) => client.query(`INSERT INTO habits(goal_habit, prereq_habit_1, prereq_habit_2, prereq_habit_3) VALUES('${data.goal_habit}', '${data.habit_1}', '${data.habit_2}', '${data.habit_3}')`);
+const addHabit = (data) => client.query(`INSERT INTO habits(habit_goal, habit_1, habit_2, habit_3) VALUES('${data.goal_habit}', '${data.habit_1}', '${data.habit_2}', '${data.habit_3}')`);
+
+const getHabitOverview = (data) => client.query(`SELECT habit_goal, habit_1, habit_2, habit_3 from habits WHERE id = ${data}`);
 
 module.exports = {
   addHabit,
+  getHabitOverview,
 };
