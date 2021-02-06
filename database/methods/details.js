a80a4c81b3ef64eacc2fc8f760deb2ae079c74df
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-02-05 17:00:45 -0800

    implemented /undo, /update, /delete endpoints

--- database/methods/details.js
+++ database/methods/details.js
@@ -4,7 +4,84 @@ const addDetails = (data) => client.query(`INSERT INTO details(day_1, day_2, day
 
 const getDetails = (data) => client.query(`SELECT id, current_habit, time_1, time_2, time_3, time_4 FROM details WHERE day_${data} = true`);
 
+const updateDetails = async (data) => {
+  let updateData = ``;
+
+  if(data.time_1) {
+    updateData += ` time_1 = '${data.time_1}' `
+  }
+  if(data.time_2) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` time_2 = '${data.time_2}' `
+  }
+  if(data.time_3) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` time_3 = '${data.time_3}' `
+  }
+  if(data.time_4) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` time_4 = '${data.time_4}' `
+  }
+  if(data.day_1) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_1 = ${data.day_1} `
+  }
+  if(data.day_2) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_2 = ${data.day_2} `
+  }
+  if(data.day_3) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_3 = ${data.day_3} `
+  }
+  if(data.day_4) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_4 = ${data.day_4} `
+  }
+  if(data.day_5) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_5 = ${data.day_5} `
+  }
+  if(data.day_6) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_6 = ${data.day_6} `
+  }
+  if(data.day_7) {
+    if(updateData.length > 0){
+      updateData += ',';
+    }
+    updateData += ` day_7 = ${data.day_7} `
+  }
+
+  return await client.query(`UPDATE details SET ${updateData} WHERE id = ${data.id}`);
+};
+
+const updateCurrentHabit = (id) => client.query(`UPDATE details SET current_habit = current_habit + 1 WHERE id = ${id}`);
+
+const deleteDetails = (id) => client.query(`DELETE FROM details WHERE id = ${id}`);
+
 module.exports = {
   addDetails,
   getDetails,
+  updateDetails,
+  updateCurrentHabit,
+  deleteDetails,
 };
--- database/methods/progress.js
+++ database/methods/progress.js
@@ -1,13 +1,23 @@
+const { cli } = require('webpack');
 const client = require('../index');
 
 const initProgress = () => client.query('INSERT INTO progress(completed) VALUES(false)');
 
 const getTodaysProgress = (data) => client.query(`SELECT * FROM progress WHERE id = ${data}`);
 
-const completeHabit = (id, data) => client.query(`UPDATE progress SET completed = ${data}, streak = streak + 1 WHERE id = ${id}`)
+const completeHabit = (id, data) => client.query(`UPDATE progress SET completed = ${data}, streak = streak + 1 WHERE id = ${id} RETURNING streak`);
+
+const undoComplete = (id) => client.query(`UPDATE progress SET completed = false, streak = streak - 1 WHERE id = ${id}`);
+
+const resetProgress = (id) => client.query(`UPDATE progress SET completed = false, streak = 0 WHERE id = ${id}`);
+
+const deleteProgress = (id) => client.query(`DELETE FROM progress WHERE id = ${id}`);
 
 module.exports = {
   initProgress,
   getTodaysProgress,
   completeHabit,
+  undoComplete,
+  resetProgress,
+  deleteProgress,
 };
