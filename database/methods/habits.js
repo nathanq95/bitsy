26ffdb55acd7fb0815be133d6510d4d71b68476c
nathanq95 <nathanquiocson@gmail.com>
2021-02-05 22:00:17 -0800

    working on integration tests

--- database/methods/habits.js
+++ database/methods/habits.js
@@ -2,7 +2,7 @@ const client = require('../index');
 
 const addHabit = (data) => client.query(`INSERT INTO habits(habit_1, habit_2, habit_3, habit_4) VALUES('${data.habit_1}', '${data.habit_2}', '${data.habit_3}', '${data.habit_4}')`);
 
-const getHabitOverview = (data) => client.query(`SELECT habit_1, habit_2, habit_3, habit_4 from habits WHERE id = ${data}`);
+const getHabitOverview = (id) => client.query(`SELECT habit_1, habit_2, habit_3, habit_4 from habits WHERE id = ${id}`);
 
 const getCurrentHabit = (id, data) => client.query(`SELECT id, habit_${data} FROM habits WHERE id = ${id} `);
 
