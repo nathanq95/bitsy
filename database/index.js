5f88c2bb61c0615893a26e1f04061c0142d8c299
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-02-07 23:19:26 -0800

    added env variable

--- database/index.js
+++ database/index.js
@@ -1,16 +1,23 @@
 const { Client } = require('pg');
+let database;
+
+if (!process.env.TEST) {
+  database = 'habits';
+} else {
+  database = process.env.TEST;
+}
 
 const client = new Client({
-  database: 'habits',
+  database: database,
   host: 'localhost',
 });
 
 client.connect()
   .then(() => {
-    console.log('Connected to the ProgreSQL database!');
+    console.log(`Connected to the ${database} database!`);
   })
   .catch((err) => {
-    console.error('Error connecting to the database: ', err.stack);
+    console.error(`Error connecting to the ${database} database: `, err.stack);
   });
 
 module.exports = client;
