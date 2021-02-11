6f52d454834753484f93f527a28963ef479c2446
nathanq95 <nathanquiocson@gmail.com>
2021-02-10 22:26:35 -0800

    refactored db methods

--- database/methods/habits.js
+++ database/methods/habits.js
@@ -1,45 +1,49 @@
 const client = require('../index');
 
-const addHabit = (data) => client.query(`INSERT INTO habits(habit_1, habit_2, habit_3, habit_4) VALUES('${data.habit_1}', '${data.habit_2}', '${data.habit_3}', '${data.habit_4}')`);
-
-const getHabitOverview = (id) => client.query(`SELECT habit_1, habit_2, habit_3, habit_4 from habits WHERE id = ${id}`);
-
-const getCurrentHabit = (id, data) => client.query(`SELECT id, habit_${data} FROM habits WHERE id = ${id} `);
-
-const updateHabit = (id, data) => {
-  let updateData = '';
-
-  if (data.habit_1) {
-    updateData += ` habit_1 = '${data.habit_1}' `;
+class Habits {
+  add(data) {
+    return client.query(`INSERT INTO habits(habit_1, habit_2, habit_3, habit_4) VALUES('${data.habit_1}', '${data.habit_2}', '${data.habit_3}', '${data.habit_4}')`);
   }
-  if (data.habit_2) {
-    if (updateData.length > 0) {
-      updateData += ',';
+
+  getOverview(id) {
+    return client.query(`SELECT habit_1, habit_2, habit_3, habit_4 FROM habits WHERE id = ${id}`);
+  }
+
+  getCurrent(id, data) {
+    return client.query(`SELECT id, habit_${data} FROM habits WHERE id = ${id}`);
+  }
+
+  update(id, data) {
+    let updateData = '';
+
+    if (data.habit_1) {
+      updateData += ` habit_1 = '${data.habit_1}' `;
     }
-    updateData += ` habit_2 = '${data.habit_2}' `;
-  }
-  if (data.habit_3) {
-    if (updateData.length > 0) {
-      updateData += ',';
+    if (data.habit_2) {
+      if (updateData.length > 0) {
+        updateData += ',';
+      }
+      updateData += ` habit_2 = '${data.habit_2}' `;
     }
-    updateData += ` habit_3 = '${data.habit_3}' `;
-  }
-  if (data.habit_4) {
-    if (updateData.length > 0) {
-      updateData += ',';
+    if (data.habit_3) {
+      if (updateData.length > 0) {
+        updateData += ',';
+      }
+      updateData += ` habit_3 = '${data.habit_3}' `;
     }
-    updateData += ` habit_4 = '${data.habit_4}' `;
+    if (data.habit_4) {
+      if (updateData.length > 0) {
+        updateData += ',';
+      }
+      updateData += ` habit_4 = '${data.habit_4}' `;
+    }
+  
+    return client.query(`UPDATE habits SET ${updateData} WHERE id = ${id}`);
   }
 
-  return client.query(`UPDATE habits SET ${updateData} WHERE id = ${id}`);
-};
+  delete(id) {
+    return client.query(`DELETE FROM habits WHERE id = ${id}`);
+  }
+}
 
-const deleteHabit = (id) => client.query(`DELETE FROM habits WHERE id = ${id}`);
-
-module.exports = {
-  addHabit,
-  getHabitOverview,
-  getCurrentHabit,
-  updateHabit,
-  deleteHabit,
-};
+module.exports = Habits;
