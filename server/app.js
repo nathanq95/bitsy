ab05a996693399c4021ab8bb67ec24c1995705d0
nathanq95 <nathanquiocson@gmail.com>
2021-02-11 11:55:20 -0800

    added middleware to check req body

--- server/app.js
+++ server/app.js
@@ -11,6 +11,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/api/add', require('./middleware/checkReqBody'));
 app.use('/api', require('./routes/habits.js'));
 
 app.get('*', (req, res) => {
