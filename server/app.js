697865d66d57a11a1a759f0b47be14278892b1ea
Nathan <nathanquiocson@Nathans-MacBook-Pro.local>
2021-02-11 15:56:16 -0800

    updated http code for /overview

--- server/app.js
+++ server/app.js
@@ -11,7 +11,13 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/', express.static(path.join(__dirname, '../client/dist')));
-app.use('/api/add', require('./middleware/checkReqBody'));
+app.use('/api/add', require('./middleware/checkData'));
+app.use('/api/update', require('./middleware/checkId'));
+app.use('/api/update', require('./middleware/checkData'));
+app.use('/api/overview', require('./middleware/checkId'));
+app.use('/api/complete', require('./middleware/checkId'));
+app.use('/api/undo', require('./middleware/checkId'));
+app.use('/api/delete', require('./middleware/checkId'));
 app.use('/api', require('./routes/habits.js'));
 
 app.get('*', (req, res) => {
