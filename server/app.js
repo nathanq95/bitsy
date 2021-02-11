dac79222834028074686e1197c6d97aa44d35ad0
nathanq95 <nathanquiocson@gmail.com>
2021-02-11 10:23:42 -0800

    extracted app, added env variable for port number

---
+++ server/app.js
@@ -0,0 +1,20 @@
+require('../database/index');
+const express = require('express');
+const path = require('path');
+const app = express();
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/', express.static(path.join(__dirname, '../client/dist')));
+app.use('/api', require('./routes/habits.js'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+});
+
+module.exports = app;
