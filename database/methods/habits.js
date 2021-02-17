b0c1c75f9076dd95205c0442aed5d77de61ab791
nathanq95 <nathanquiocson@gmail.com>
2021-02-17 03:09:23 -0800

    refactored db methods to handle errors

--- database/methods/habits.js
+++ database/methods/habits.js
@@ -2,47 +2,80 @@ const client = require('../index');
 
 class Habits {
   add(data) {
-    return client.query(`INSERT INTO habits(habit_1, habit_2, habit_3, habit_4) VALUES('${data.habit_1}', '${data.habit_2}', '${data.habit_3}', '${data.habit_4}')`);
+    if (typeof data === 'object') {
+      try {
+        return client.query(`INSERT INTO habits(habit_1, habit_2, habit_3, habit_4) VALUES('${data.habit_1}', '${data.habit_2}', '${data.habit_3}', '${data.habit_4}')`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    } else {
+      throw new Error('Invalid argument type');
+    }
   }
 
   getOverview(id) {
-    return client.query(`SELECT habit_1, habit_2, habit_3, habit_4 FROM habits WHERE id = ${id}`);
+    if (typeof id === 'number') {
+      try {
+        return client.query(`SELECT habit_1, habit_2, habit_3, habit_4 FROM habits WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    } else {
+      throw new Error('Invalid argument type');
+    }
+    
   }
 
   getCurrent(id, data) {
-    return client.query(`SELECT id, habit_${data} FROM habits WHERE id = ${id}`);
+    if (typeof id !== 'number' || typeof data !== 'number') {
+      throw new Error('Invalid argument type');
+    } else {
+      return client.query(`SELECT id, habit_${data} FROM habits WHERE id = ${id}`);
+    }
   }
 
   update(id, data) {
-    let updateData = '';
-
-    if (data.habit_1) {
-      updateData += ` habit_1 = '${data.habit_1}' `;
-    }
-    if (data.habit_2) {
-      if (updateData.length > 0) {
-        updateData += ',';
-      }
-      updateData += ` habit_2 = '${data.habit_2}' `;
-    }
-    if (data.habit_3) {
-      if (updateData.length > 0) {
-        updateData += ',';
-      }
-      updateData += ` habit_3 = '${data.habit_3}' `;
-    }
-    if (data.habit_4) {
-      if (updateData.length > 0) {
-        updateData += ',';
-      }
-      updateData += ` habit_4 = '${data.habit_4}' `;
-    }
+    if (typeof id !== 'number' || typeof data !== 'object') {
+      throw new Error('Invalid argument type');
+    } else {
+      let updateData = '';
   
-    return client.query(`UPDATE habits SET ${updateData} WHERE id = ${id}`);
+      if (data.habit_1) {
+        updateData += ` habit_1 = '${data.habit_1}' `;
+      }
+      if (data.habit_2) {
+        if (updateData.length > 0) {
+          updateData += ',';
+        }
+        updateData += ` habit_2 = '${data.habit_2}' `;
+      }
+      if (data.habit_3) {
+        if (updateData.length > 0) {
+          updateData += ',';
+        }
+        updateData += ` habit_3 = '${data.habit_3}' `;
+      }
+      if (data.habit_4) {
+        if (updateData.length > 0) {
+          updateData += ',';
+        }
+        updateData += ` habit_4 = '${data.habit_4}' `;
+      }
+    
+      return client.query(`UPDATE habits SET ${updateData} WHERE id = ${id}`);
+    }
   }
 
   delete(id) {
-    return client.query(`DELETE FROM habits WHERE id = ${id}`);
+    if (typeof id !== 'number') {
+      throw new Error('Invalid argument type');
+    } else {
+      try {
+        return client.query(`DELETE FROM habits WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 }
 
