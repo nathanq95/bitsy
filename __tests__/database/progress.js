c86189cc1e9cf5a6ec60862694a05004e6b30cd1
nathanq95 <nathanquiocson@gmail.com>
2021-02-18 00:07:27 -0800

    implemented error handling for progress table

--- __tests__/database/progress.js
+++ __tests__/database/progress.js
@@ -1,11 +1,10 @@
-const { expect } = require('chai');
 const Progress = require('../../database/methods/progress');
 
 describe ('progress table methods', () => {
   const progress = new Progress();
 
   describe ('init', () => {
-    it ('should generate initial data in the progress table', async (done) => {
+    it ('should run an INSERT query on the progress table', async (done) => {
       const initProgressTest = await progress.init();
 
       expect(initProgressTest).to.equal('INSERT INTO progress(completed) VALUES(false)');
@@ -14,51 +13,131 @@ describe ('progress table methods', () => {
   });
 
   describe ('get', () => {
-    it ('should return all data', async (done) => {
+    it ('should run a SELECT query on the progress table', async (done) => {
       const id = 1;
       const getProgressTest = await progress.get(id);
 
       expect(getProgressTest).to.equal('SELECT * FROM progress WHERE id = 1');
       done();
     });
+
+    it ('should throw an error if id is not of type number', (done) => {
+      const id = '1';
+
+      const getProgressTest = () => progress.get(id);
+
+      expect(getProgressTest).toThrow('Invalid argument type');
+      done();
+    });
+
+    it ('should throw an error if id is undefined', (done) => {
+      const getProgressTest = () => progress.get();
+
+      expect(getProgressTest).toThrow('Missing argument');
+      done();
+    });
   });
 
   describe ('complete', () => {
-    it ('should set the completed column to true', async (done) => {
+    it ('should run an UPDATE query on the progress table', async (done) => {
       const id = 1;
       const completeProgressTest = await progress.complete(id);
 
       expect(completeProgressTest).to.equal('UPDATE progress SET completed = true, streak = streak + 1 WHERE id = 1 RETURNING streak');
       done();
     });
+
+    it ('should throw an error if id is not of type number', (done) => {
+      const id = '1';
+
+      const completeProgressTest = () => progress.complete(id);
+
+      expect(completeProgressTest).toThrow('Invalid argument type');
+      done();
+    });
+
+    it ('should throw an error if id is undefined', (done) => {
+      const completeProgressTest = () => progress.complete();
+
+      expect(completeProgressTest).toThrow('Missing argument');
+      done();
+    });
   });
 
   describe ('undoComplete', () => {
-    it ('should set the completed column to false', async (done) => {
+    it ('should run an UPDATE query on the progress table', async (done) => {
       const id = 1;
       const undoCompleteTest = await progress.undoComplete(id);
 
       expect(undoCompleteTest).to.equal('UPDATE progress SET completed = false, streak = streak - 1 WHERE id = 1');
       done();
     });
+
+    it ('should throw an error if id is undefined', (done) => {
+      const undoCompleteTest = () => progress.undoComplete();
+
+      expect(undoCompleteTest).toThrow('Missing argument');
+      done();
+    });
+
+    it ('should throw an error if id is not of type number', (done) => {
+      const id = '1';
+
+      const undoCompleteTest = () => progress.undoComplete(id);
+
+      expect(undoCompleteTest).toThrow('Invalid argument type');
+      done();
+    });
   });
 
   describe ('reset', () => {
-    it ('should reset the completed & streak columns to their initial values', async (done) => {
+    it ('should run an UPDATE query to reset the completed & streak columns to their initial values', async (done) => {
       const id = 1;
       const resetProgressTest = await progress.reset(id);
 
       expect(resetProgressTest).to.equal('UPDATE progress SET completed = false, streak = 0 WHERE id = 1');
       done();
     });
+
+    it ('should throw an error if id is undefined', (done) => {
+      const resetProgressTest = () => progress.reset();
+
+      expect(resetProgressTest).toThrow('Missing argument');
+      done();
+    });
+
+    it ('should throw an error if id is not of type number', (done) => {
+      const id = '1';
+
+      const resetProgressTest = () => progress.reset(id);
+
+      expect(resetProgressTest).toThrow('Invalid argument type');
+      done();
+    });
   });
   
   describe ('delete', () => {
-    it ('should delete data from the progress table', async (done) => {
+    it ('should run a DELETE query on the progress table', async (done) => {
       const id = 1;
       const deleteProgressTest = await progress.delete(id);
 
       expect(deleteProgressTest).to.equal('DELETE FROM progress WHERE id = 1');
+      done();
+    });
+
+    it ('should throw an error if id is undefined', (done) => {
+      const deleteProgressTest = () => progress.delete();
+
+      expect(deleteProgressTest).toThrow('Missing argument');
+      done();
+    });
+
+    it ('should throw an error if id is not of type number', (done) => {
+      const id = '1';
+
+      const deleteProgressTest = () => progress.delete(id);
+
+      expect(deleteProgressTest).toThrow('Invalid argument type');
       done();
     });
   });
--- database/methods/progress.js
+++ database/methods/progress.js
@@ -5,24 +5,84 @@ class Progress {
     return client.query('INSERT INTO progress(completed) VALUES(false)');
   }
 
-  get(data) {
-    return client.query(`SELECT * FROM progress WHERE id = ${data}`);
+  get(id) {
+    if (typeof id !== 'number') {
+      if (id) {
+        throw new Error('Invalid argument type');
+      } else {
+        throw new Error('Missing argument');
+      }
+    } else {
+      try {
+        return client.query(`SELECT * FROM progress WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 
   complete(id) {
-    return client.query(`UPDATE progress SET completed = true, streak = streak + 1 WHERE id = ${id} RETURNING streak`);
+    if (typeof id !== 'number') {
+      if (id) {
+        throw new Error('Invalid argument type');
+      } else {
+        throw new Error('Missing argument');
+      }
+    } else {
+      try {
+        return client.query(`UPDATE progress SET completed = true, streak = streak + 1 WHERE id = ${id} RETURNING streak`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 
   undoComplete(id) {
-    return client.query(`UPDATE progress SET completed = false, streak = streak - 1 WHERE id = ${id}`);
+    if (typeof id !== 'number') {
+      if (id) {
+        throw new Error('Invalid argument type');
+      } else {
+        throw new Error('Missing argument');
+      }
+    } else {
+      try {
+        return client.query(`UPDATE progress SET completed = false, streak = streak - 1 WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 
   reset(id) {
-    return client.query(`UPDATE progress SET completed = false, streak = 0 WHERE id = ${id}`);
+    if (typeof id !== 'number') {
+      if (id) {
+        throw new Error('Invalid argument type');
+      } else {
+        throw new Error('Missing argument');
+      }
+    } else {
+      try {
+        return client.query(`UPDATE progress SET completed = false, streak = 0 WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 
   delete(id) {
-    return client.query(`DELETE FROM progress WHERE id = ${id}`);
+    if (typeof id !== 'number') {
+      if (id) {
+        throw new Error('Invalid argument type');
+      } else {
+        throw new Error('Missing argument');
+      }
+    } else {
+      try {
+        return client.query(`DELETE FROM progress WHERE id = ${id}`);
+      } catch (err) {
+        throw new Error(err);
+      }
+    }
   }
 }
 
