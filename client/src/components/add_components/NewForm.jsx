60283193e18c897986aff19232a32bbe8dbba170
nathanq95 <nathanquiocson@gmail.com>
2021-03-11 00:37:55 -0800

    fixed bug where form sent with missing values

--- client/src/components/add_components/NewForm.jsx
+++ client/src/components/add_components/NewForm.jsx
@@ -17,10 +17,10 @@ const NewForm = (props) => (
         fixed
         maxWidth="xs"
         className="new">
-        <TextField className="habit" id="bit4" label="Goal Habit" required/>
-        <TextField className="habit" id="bit1" label="bit 1" required/>
-        <TextField className="habit" id="bit2" label="bit 2" required/>
-        <TextField className="habit" id="bit3" label="bit 3" required/>
+        <TextField className="habit" type="text" id="bit4" label="Goal Habit" required/>
+        <TextField className="habit" type="text" id="bit1" label="bit 1" required/>
+        <TextField className="habit" type="text" id="bit2" label="bit 2" required/>
+        <TextField className="habit" type="text" id="bit3" label="bit 3" required/>
         <div className="day-selector">
           <InputLabel shrink={false}>Every</InputLabel>
           <Select
@@ -31,12 +31,13 @@ const NewForm = (props) => (
             },
             getContentAnchorEl: null
           }}
-            labelId="goal"
-            id="selected-day"
-            value={props.selectedDays}
-            onChange={(e) => props.handleChange(e)}
-            multiple
-            renderValue={(selected) => selected.join(', ')}
+          labelId="goal"
+          id="selected-day"
+          value={props.selectedDays}
+          onChange={(e) => props.handleChange(e)}
+          multiple
+          renderValue={(selected) => selected.join(', ')}
+          required
           >
             {dayNames.map((day) => (
               <MenuItem key={day} value={day}>
